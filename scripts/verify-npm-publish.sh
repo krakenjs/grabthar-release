78d14d8ff4657fd57c16d309fea12aaef8eb2cd1
Nate Bierdeman <nbierdeman@paypal.com>
2020-09-21 11:04:55 -0500

    Add local variables to verify_npm_publish and update variable names

--- scripts/verify-npm-publish.sh
+++ scripts/verify-npm-publish.sh
@@ -11,17 +11,22 @@ local_version=$(node --eval "
 ")
 
 verify_npm_publish () {
-    npm_public_registry_version_latest_tag=$(npm view $1 version latest);
-    echo "package name: $1"
-    echo "local version: $2"
-    echo "npm version: $npm_public_registry_version_latest_tag\n"
-    while [ "$2" != "$npm_public_registry_version_latest_tag" ]
-    do
-        echo "Version mismatch. Trying again in 5 seconds...\n"
-        sleep 5;
-        npm_public_registry_version_latest_tag=$(npm view $1 version latest);
-    done
-    echo "Successful version match."
+  package_name=$1
+  local_version=$2
+  dist_tag=$3
+
+  npm_public_registry_version=$(npm view $package_name version $dist_tag);
+
+  echo "package name: $package_name"
+  echo "local version: $local_version"
+  echo "npm version: $npm_public_registry_version\n"
+  while [ "$local_version" != "$npm_public_registry_version" ]
+  do
+      echo "Version mismatch. Trying again in 5 seconds...\n"
+      sleep 5;
+      npm_public_registry_version=$(npm view $package_name version $dist_tag);
+  done
+  echo "Successful version match."
 }
 
-verify_npm_publish "$module" "$local_version"
+verify_npm_publish "$package_name" "$local_version" "$dist_tag"
