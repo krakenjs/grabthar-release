f4987c7d4089db7aedeec455862221dfdb5d9d0b
Nate Bierdeman <nbierdeman@paypal.com>
2020-09-21 11:13:11 -0500

    Remove unnecessary local variables

--- scripts/verify-npm-publish.sh
+++ scripts/verify-npm-publish.sh
@@ -11,22 +11,20 @@ local_version=$(node --eval "
 ")
 
 verify_npm_publish () {
-  package_name=$1
-  local_version=$2
-  dist_tag=$3
+    dist_tag=$1
 
-  npm_public_registry_version=$(npm view $package_name version $dist_tag);
+    npm_public_registry_version=$(npm view $package_name version $dist_tag);
 
-  echo "package name: $package_name"
-  echo "local version: $local_version"
-  echo "npm version: $npm_public_registry_version\n"
-  while [ "$local_version" != "$npm_public_registry_version" ]
-  do
-      echo "Version mismatch. Trying again in 5 seconds...\n"
-      sleep 5;
-      npm_public_registry_version=$(npm view $package_name version $dist_tag);
-  done
-  echo "Successful version match."
+    echo "package name: $package_name"
+    echo "local version: $local_version"
+    echo "npm version: $npm_public_registry_version\n"
+    while [ "$local_version" != "$npm_public_registry_version" ]
+    do
+        echo "Version mismatch. Trying again in 5 seconds...\n"
+        sleep 5;
+        npm_public_registry_version=$(npm view $package_name version $dist_tag);
+    done
+    echo "Successful version match."
 }
 
-verify_npm_publish "$package_name" "$local_version" "$dist_tag"
+verify_npm_publish "$dist_tag"
