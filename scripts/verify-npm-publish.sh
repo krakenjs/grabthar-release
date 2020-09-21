4689f6d1ee2db3efd4ed3de748720fb5871140d2
Nate Bierdeman <nbierdeman@paypal.com>
2020-09-21 11:25:47 -0500

    Add default value for dist_tag variable and remove verify_npm_publish

--- scripts/verify-npm-publish.sh
+++ scripts/verify-npm-publish.sh
@@ -1,3 +1,6 @@
+# default to latest if no dist tag is passed in
+dist_tag=${1:-latest}
+
 package_name=$(node --eval "
     const PACKAGE = './package.json';
     let pkg = require(PACKAGE);
@@ -10,21 +13,17 @@ local_version=$(node --eval "
     console.log(pkg.version);
 ")
 
-verify_npm_publish () {
-    dist_tag=$1
+npm_public_registry_version=$(npm view $package_name version $dist_tag);
 
+echo "package name: $package_name"
+echo "dist tag: $dist_tag"
+echo "local version: $local_version"
+echo "npm version: $npm_public_registry_version\n"
+
+while [ "$local_version" != "$npm_public_registry_version" ]
+do
+    echo "Version mismatch. Trying again in 5 seconds...\n"
+    sleep 5;
     npm_public_registry_version=$(npm view $package_name version $dist_tag);
-
-    echo "package name: $package_name"
-    echo "local version: $local_version"
-    echo "npm version: $npm_public_registry_version\n"
-    while [ "$local_version" != "$npm_public_registry_version" ]
-    do
-        echo "Version mismatch. Trying again in 5 seconds...\n"
-        sleep 5;
-        npm_public_registry_version=$(npm view $package_name version $dist_tag);
-    done
-    echo "Successful version match."
-}
-
-verify_npm_publish "$dist_tag"
+done
+echo "Successful version match."
