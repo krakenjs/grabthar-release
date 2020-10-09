0b6743cd4ae354177357efb462cb807af5a798fd
Daniel Brain <dbrain@paypal.com>
2020-10-09 12:54:55 -0700

    Only validate flat when package lock exists

--- scripts/validate-flat.js
+++ scripts/validate-flat.js
@@ -4,19 +4,22 @@
 
 import { join } from 'path';
 
-import { readFileSync } from 'fs-extra';
+import { readFileSync, existsSync } from 'fs-extra';
 
 const PACKAGE_LOCK = join(process.cwd(), 'package-lock.json');
-const pkgLock = JSON.parse(readFileSync(PACKAGE_LOCK));
 
-for (const depName of Object.keys(pkgLock.dependencies)) {
-    const dep = pkgLock.dependencies[depName];
+if (existsSync(PACKAGE_LOCK)) {
+    const pkgLock = JSON.parse(readFileSync(PACKAGE_LOCK));
 
-    if (dep.dev) {
-        continue;
-    }
+    for (const depName of Object.keys(pkgLock.dependencies)) {
+        const dep = pkgLock.dependencies[depName];
 
-    if (dep.dependencies) {
-        throw new Error(`Expected ${  depName } to not have any unflattened sub-dependencies - found ${ Object.keys(dep.dependencies).join(', ') }`);
+        if (dep.dev) {
+            continue;
+        }
+
+        if (dep.dependencies) {
+            throw new Error(`Expected ${  depName } to not have any unflattened sub-dependencies - found ${ Object.keys(dep.dependencies).join(', ') }`);
+        }
     }
 }
