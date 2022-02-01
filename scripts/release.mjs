08714e3b608bd68af73257b197c5707d2bf30868
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-01 13:32:27 -0700

    Import argv and cwd from process

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,4 +1,5 @@
 #!/usr/bin/env zx
+import { argv, cwd } from 'process'
 
 await $`set -e`
 
@@ -7,10 +8,10 @@ const DIR = __dirname
 await $`zx ${DIR}/grabthar-validate-git`
 await $`zx ${DIR}/grabthar-validate-npm`
 
-let DIST_TAG = process.argv.find(element => element.includes('DIST_TAG='))?.replace('DIST_TAG=', '')
+let DIST_TAG = argv.find(element => element.includes('DIST_TAG='))?.replace('DIST_TAG=', '')
 DIST_TAG ?? (DIST_TAG = 'latest')
 
-let BUMP = process.argv.find(element => element.includes('BUMP='))?.replace('BUMP=', '')
+let BUMP = argv.find(element => element.includes('BUMP='))?.replace('BUMP=', '')
 BUMP ?? (BUMP = 'patch')
 
 await $`npm version ${BUMP}`
@@ -18,15 +19,15 @@ await $`git push`
 await $`git push --tags`
 await $`zx ${DIR}/grabthar-flatten`
 
-let NPM_TOKEN = process.argv.find(element => element.includes('NPM_TOKEN='))?.replace('NPM_TOKEN=', '')
+let NPM_TOKEN = argv.find(element => element.includes('NPM_TOKEN='))?.replace('NPM_TOKEN=', '')
 NPM_TOKEN ?? (NPM_TOKEN = '')
 
 await $`NPM_TOKEN=${NPM_TOKEN} npm publish --tag ${DIST_TAG}`
 await $`git checkout package.json`
 await $`git checkout package-lock.json || echo 'Package lock not found'`
 
-const cwd = process.cwd()
-const local_version = require(`${cwd}/package.json`).version
+const current_working_directory = cwd()
+const local_version = require(`${current_working_directory}/package.json`).version
 
 await $`zx ${DIR}/grabthar-verify-npm-publish ${local_version} ${DIST_TAG}`
 
