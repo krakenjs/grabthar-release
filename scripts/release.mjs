283c72a00437060eac61d01f2776f421da7304c7
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-01 10:14:22 -0700

    Get DIST_TAG from command line or set it to latest

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -5,3 +5,7 @@ await $`set -e`
 const DIR = __dirname
 await $`zx ${DIR}/grabthar-validate-git`
 await $`zx ${DIR}/grabthar-validate-npm`
+
+let DIST_TAG = process.argv.find(element => element.includes('DIST_TAG='))?.replace('DIST_TAG=', '')
+
+DIST_TAG ?? (DIST_TAG = 'latest')
