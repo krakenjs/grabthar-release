4b5c71ec559f760325b44e61c38ed9f9517d943c
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-01 11:28:45 -0700

    Get NPM_TOKEN from command line or set it to an empty string

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -3,6 +3,7 @@
 await $`set -e`
 
 const DIR = __dirname
+
 await $`zx ${DIR}/grabthar-validate-git`
 await $`zx ${DIR}/grabthar-validate-npm`
 
@@ -13,7 +14,9 @@ let BUMP = process.argv.find(element => element.includes('BUMP='))?.replace('BUM
 BUMP ?? (BUMP = 'patch')
 
 await $`npm version ${BUMP}`
-
 await $`git push`
 await $`git push --tags`
 await $`zx ${DIR}/grabthar-flatten`
+
+let NPM_TOKEN = process.argv.find(element => element.includes('NPM_TOKEN='))?.replace('NPM_TOKEN=', '')
+NPM_TOKEN ?? (NPM_TOKEN = '')
