7cc102ceeda5bbcc70e49c0546f5ce768fc67358
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-03 16:41:37 -0700

    Add missing semicolons

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,9 +1,9 @@
 #!/usr/bin/env zx
-import { argv, cwd } from 'process'
+import { argv, cwd } from 'process';
 
-await $`set -e`
+await $`set -e`;
 
-const DIR = __dirname
+const DIR = __dirname;
 const args = {};
 
 for (const element of argv) {
@@ -19,22 +19,22 @@ DIST_TAG ?? (DIST_TAG = 'latest');
 BUMP ?? (BUMP = 'patch');
 NPM_TOKEN ?? (NPM_TOKEN = '');
 
-await $`${DIR}/grabthar-validate-git`
-await $`${DIR}/grabthar-validate-npm`
-await $`npm version ${BUMP}`
-await $`git push`
-await $`git push --tags`
-await $`${DIR}/grabthar-flatten`
-await $`NPM_TOKEN=${NPM_TOKEN} npm publish --tag ${DIST_TAG}`
-await $`git checkout package.json`
-await $`git checkout package-lock.json || echo 'Package lock not found'`
+await $`${DIR}/grabthar-validate-git`;
+await $`${DIR}/grabthar-validate-npm`;
+await $`npm version ${BUMP}`;
+await $`git push`;
+await $`git push --tags`;
+await $`${DIR}/grabthar-flatten`;
+await $`NPM_TOKEN=${NPM_TOKEN} npm publish --tag ${DIST_TAG}`;
+await $`git checkout package.json`;
+await $`git checkout package-lock.json || echo 'Package lock not found'`;
 
-const current_working_directory = cwd()
-const local_version = require(`${current_working_directory}/package.json`).version
+const current_working_directory = cwd();
+const local_version = require(`${current_working_directory}/package.json`).version;
 
-await $`${DIR}/grabthar-verify-npm-publish ${local_version} ${DIST_TAG}`
+await $`${DIR}/grabthar-verify-npm-publish ${local_version} ${DIST_TAG}`;
 
 // update non-prod dist tags whenever the latest dist tag changes
 if (DIST_TAG === 'latest') {
-  await $`${DIR}/grabthar-activate LOCAL_VERSION=${local_version} CDNIFY=false TAGS='[test, local, stage]'`
+  await $`${DIR}/grabthar-activate LOCAL_VERSION=${local_version} CDNIFY=false TAGS='[test, local, stage]'`;
 }
