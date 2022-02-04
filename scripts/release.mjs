0c174da8d59322ab19f0467f842f82305268ff4d
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-04 11:47:56 -0700

    Use command line args formatted for minimist

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -28,5 +28,5 @@ await $`${DIR}/grabthar-verify-npm-publish ${local_version} ${DIST_TAG}`;
 
 // update non-prod dist tags whenever the latest dist tag changes
 if (DIST_TAG === 'latest') {
-  await $`${DIR}/grabthar-activate LOCAL_VERSION=${local_version} CDNIFY=false TAGS='[test, local, stage]'`;
+  await $`${DIR}/grabthar-activate --LOCAL_VERSION=${local_version} --CDNIFY=false --TAGS=test,local,stage`;
 }
