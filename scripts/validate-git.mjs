61d57526e28ef193cc387cf60055968a1fda845f
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-07 10:19:56 -0700

    Throw error and exit if there are unstaged changes

--- scripts/validate-git.mjs
+++ scripts/validate-git.mjs
@@ -1,1 +1,8 @@
 #!/usr/bin/env zx
+
+try {
+  await $`git diff-files --quiet`;
+} catch (p) {
+  console.log('ERROR: Cannot continue with unstaged changes');
+  console.log(`Exit code: ${p.exitCode}`);
+}
