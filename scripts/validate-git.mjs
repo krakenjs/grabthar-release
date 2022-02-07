558c36278dac29d3fb851a0a1f3f8f8c1bb6ed72
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-07 15:22:45 -0700

    Use object destructuring assignment where possible

--- scripts/validate-git.mjs
+++ scripts/validate-git.mjs
@@ -11,14 +11,11 @@ await $`npx check-node-version --node='>=14.13.1' --npm='>=6.14'`;
 
 const UPSTREAM = 'origin';
 let LOCAL_BRANCH = await $`git rev-parse --abbrev-ref HEAD`;
-let LOCAL_COMMIT = await $`git rev-parse HEAD`;
-let REMOTE_COMMIT = await $`git rev-parse ${UPSTREAM}/${LOCAL_BRANCH}`;
-let BASE_COMMIT = await $`git merge-base HEAD ${UPSTREAM}/${LOCAL_BRANCH}`;
+const { stdout: LOCAL_COMMIT } = await $`git rev-parse HEAD`;
+const { stdout: REMOTE_COMMIT } = await $`git rev-parse ${UPSTREAM}/${LOCAL_BRANCH}`;
+const { stdout: BASE_COMMIT } = await $`git merge-base HEAD ${UPSTREAM}/${LOCAL_BRANCH}`;
 
 LOCAL_BRANCH = LOCAL_BRANCH.stdout;
-LOCAL_COMMIT = LOCAL_COMMIT.stdout;
-REMOTE_COMMIT = REMOTE_COMMIT.stdout;
-BASE_COMMIT = BASE_COMMIT.stdout;
 
 if (LOCAL_COMMIT !== REMOTE_COMMIT) {
   if (LOCAL_COMMIT === BASE_COMMIT) {
