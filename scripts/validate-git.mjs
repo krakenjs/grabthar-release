a3f9d93da37e2d8224bb7f5590cbd6b29d102f95
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-07 10:42:55 -0700

    Throw error and exit if there are uncommitted changes

--- scripts/validate-git.mjs
+++ scripts/validate-git.mjs
@@ -2,7 +2,8 @@
 
 try {
   await $`git diff-files --quiet`;
+  await $`git diff-index --quiet --cached HEAD`;
 } catch (p) {
-  console.log('ERROR: Cannot continue with unstaged changes');
+  console.log('ERROR: Cannot continue with unstaged or uncommitted changes');
   console.log(`Exit code: ${p.exitCode}`);
 }
