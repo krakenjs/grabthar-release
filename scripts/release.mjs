9660ce32ec1529855a25a0ac56bfb48e8042e56a
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-08 10:43:21 -0700

    Rename current_working_directory to CWD for the sake of brevity

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -20,8 +20,8 @@ await $`NPM_TOKEN=${NPM_TOKEN} npm publish --tag ${DIST_TAG}`;
 await $`git checkout package.json`;
 await $`git checkout package-lock.json || echo 'Package lock not found'`;
 
-const current_working_directory = cwd();
-const { version: local_version } = require(`${current_working_directory}/package.json`);
+const CWD = cwd();
+const { version: local_version } = require(`${CWD}/package.json`);
 
 await $`${DIR}/grabthar-verify-npm-publish ${local_version} ${DIST_TAG}`;
 
