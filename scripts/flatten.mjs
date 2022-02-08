a4e600ba287229d7f80a08be1960e78dff1d599a
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-08 16:18:51 -0700

    Check for package.json and package-lock.json in the file system

--- scripts/flatten.mjs
+++ scripts/flatten.mjs
@@ -4,3 +4,16 @@ const DIR = __dirname;
 
 await $`${DIR}/grabthar-validate-git`;
 await $`${DIR}/grabthar-validate-flat`;
+
+const fs = require('fs');
+
+const PACKAGE = './package.json';
+const PACKAGE_LOCK = './package-lock.json';
+
+if (!fs.existsSync(PACKAGE)) {
+    throw new Error('Expected package.json to be present');
+}
+
+if (!fs.existsSync(PACKAGE_LOCK)) {
+    process.exit(0);
+}
