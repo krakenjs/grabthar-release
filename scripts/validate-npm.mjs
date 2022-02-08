c1bbbc028d21346b715879d5fdb674f387170835
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-08 10:41:22 -0700

    Get org name

--- scripts/validate-npm.mjs
+++ scripts/validate-npm.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-import { env } from 'process';
+import { cwd, env } from 'process';
 
 const NPM_TOKEN = env.NPM_TOKEN;
 let whoAmI;
@@ -16,4 +16,12 @@ if (whoAmI) {
 } else {
   console.log("You must be logged in to publish a release. Running 'npm login'.");
   await $`npm login`;
+}
+
+const CWD = cwd();
+const PACKAGE = require(`${CWD}/package.json`);
+let org;
+
+if (PACKAGE.name.indexOf('@') === 0) {
+  org = PACKAGE.name.split('/')[0].slice(1);
 }
