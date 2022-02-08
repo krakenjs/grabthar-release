ddd985c15f80510c7681ddbb9789240cb08311e6
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-08 09:50:23 -0700

    Get username or prompt to log in

--- scripts/validate-npm.mjs
+++ scripts/validate-npm.mjs
@@ -1,1 +1,19 @@
 #!/usr/bin/env zx
+
+import { env } from 'process';
+
+const NPM_TOKEN = env.NPM_TOKEN;
+let whoAmI;
+
+if (NPM_TOKEN) {
+  whoAmI = await $`NPM_TOKEN=${NPM_TOKEN} npm whoami`;
+} else {
+  whoAmI = await $`npm whoami`;
+}
+
+if (whoAmI) {
+  console.log(`npm username: ${whoAmI}`);
+} else {
+  console.log("You must be logged in to publish a release. Running 'npm login'.");
+  await $`npm login`;
+}
