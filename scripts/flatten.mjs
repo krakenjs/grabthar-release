3cc246a28aa6e560de76c3da2e738cef6925c9e1
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-08 16:33:35 -0700

    Remove whitespace

--- scripts/flatten.mjs
+++ scripts/flatten.mjs
@@ -11,10 +11,10 @@ const PACKAGE = './package.json';
 const PACKAGE_LOCK = './package-lock.json';
 
 if (!fs.existsSync(PACKAGE)) {
-    throw new Error('Expected package.json to be present.');
+  throw new Error('Expected package.json to be present.');
 }
 
 if (!fs.existsSync(PACKAGE_LOCK)) {
-    console.log('Error: Expected package-lock.json to be present.');
-    process.exit(0);
+  console.log('Error: Expected package-lock.json to be present.');
+  process.exit(0);
 }
