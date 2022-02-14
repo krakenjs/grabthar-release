5123ed73e88600ef01062c5c8b80600c323ef9ae
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-14 11:30:40 -0700

    Throw error if DIST_TAG or LOCAL_VERSION aren't passed to script

--- scripts/verify-npm-publish.mjs
+++ scripts/verify-npm-publish.mjs
@@ -1,1 +1,9 @@
 #!/usr/bin/env zx
+
+import 'zx/globals';
+
+let { DIST_TAG, LOCAL_VERSION } = argv;
+
+if (!DIST_TAG || !LOCAL_VERSION) {
+  throw new Error('Invalid arguments. Expected <version> and <dist-tag>.');
+}
