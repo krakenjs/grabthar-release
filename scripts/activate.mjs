b9a917aeaa1d198f03db29e870625588e2ed0690
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-14 16:20:46 -0700

    Set up script variables

--- scripts/activate.mjs
+++ scripts/activate.mjs
@@ -1,6 +1,14 @@
 #!/usr/bin/env zx
 
+import { cwd } from 'process';
+import 'zx/globals';
+
+const { LOCAL_VERSION, CDNIFY, TAGS } = argv;
 const DIR = __dirname;
+const TAG = 'active';
+const DEFENVS = ['test', 'local', 'stage', 'sandbox', 'production'];
+const CWD = cwd();
+const { name: MODULE } = require(`${CWD}/package.json`);
 
 await $`${DIR}/grabthar-validate-git`;
 await $`${DIR}/grabthar-validate-npm`;
