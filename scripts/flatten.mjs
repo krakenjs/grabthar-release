20b6bb0f90e3b00215b9b2d075410cc0eec38ed0
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-14 09:44:37 -0700

    Use current working directory for file paths

--- scripts/flatten.mjs
+++ scripts/flatten.mjs
@@ -1,14 +1,16 @@
 #!/usr/bin/env zx
 
+import { cwd } from 'process';
+
 const DIR = __dirname;
 
 await $`${DIR}/grabthar-validate-git`;
 await $`${DIR}/grabthar-validate-flat`;
 
 const fs = require('fs');
-
-const PACKAGE = './package.json';
-const PACKAGE_LOCK = './package-lock.json';
+const CWD = cwd();
+const PACKAGE = `${CWD}/package.json`;
+const PACKAGE_LOCK = `${CWD}/package-lock.json`;
 
 if (!fs.existsSync(PACKAGE)) {
   throw new Error('Expected package.json to be present.');
