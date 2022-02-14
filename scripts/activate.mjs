da69a6c00857b69d4b9588c99adb51eb42e9587a
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-14 16:40:30 -0700

    Set local version to npm version if falsy

--- scripts/activate.mjs
+++ scripts/activate.mjs
@@ -12,3 +12,7 @@ const { name: MODULE } = require(`${CWD}/package.json`);
 
 await $`${DIR}/grabthar-validate-git`;
 await $`${DIR}/grabthar-validate-npm`;
+
+if (!LOCAL_VERSION) {
+  LOCAL_VERSION = await $`npm view ${MODULE} version`;
+}
