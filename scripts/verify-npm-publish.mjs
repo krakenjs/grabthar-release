9707ee6006ff9254eb25b810db26a1c7a38eb0c0
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-14 11:49:21 -0700

    Log package name, dist-tag, and local version to the console

--- scripts/verify-npm-publish.mjs
+++ scripts/verify-npm-publish.mjs
@@ -1,9 +1,19 @@
 #!/usr/bin/env zx
 
+import { cwd } from 'process';
 import 'zx/globals';
 
-let { DIST_TAG, LOCAL_VERSION } = argv;
+const { DIST_TAG, LOCAL_VERSION } = argv;
 
 if (!DIST_TAG || !LOCAL_VERSION) {
   throw new Error('Invalid arguments. Expected <version> and <dist-tag>.');
 }
+
+const CWD = cwd();
+const { name: PACKAGE_NAME } = require(`${CWD}/package.json`);
+
+console.table([{
+  'package name': PACKAGE_NAME,
+  'dist-tag': DIST_TAG,
+  'local version': LOCAL_VERSION
+}]);
