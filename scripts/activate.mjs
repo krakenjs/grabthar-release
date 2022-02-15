360e10548a57f718c8a0f8ef7e2e76865b22c794
Nate Bierdeman <nbierdeman@paypal.com>
2022-02-15 09:50:12 -0700

    Check that each env passed to activate script is allowed

--- scripts/activate.mjs
+++ scripts/activate.mjs
@@ -3,8 +3,7 @@
 import { cwd } from 'process';
 import 'zx/globals';
 
-let { LOCAL_VERSION, CDNIFY } = argv;
-const { ENVS } = argv;
+let { LOCAL_VERSION, CDNIFY, ENVS } = argv;
 const DIR = __dirname;
 const TAG = 'active';
 const DEFENVS = ['test', 'local', 'stage', 'sandbox', 'production'];
@@ -20,4 +19,15 @@ if (!LOCAL_VERSION) {
 
 if (!CDNIFY) {
   CDNIFY = true;
+}
+
+if (!ENVS) {
+  ENVS = DEFENVS;
+} else {
+  ENVS = ENVS.split(',');
+  for (let env of ENVS) {
+    if (!DEFENVS.includes(env)) {
+      throw new Error(`Invalid env: ${env}`);
+    }
+  }
 }
