f7284a9aef12ba978c00811d043556246ee5cd14
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-04 10:55:37 -0700

    Revert: Add alpha release capabilities to publish.sh

--- publish.sh
+++ publish.sh
@@ -13,28 +13,8 @@ if ! git diff-index --quiet --cached HEAD; then
 fi;
 
 npm test;
+npm version ${1-patch};
 
-# This will determine the type of release based on the git branch. When the default branch is used, it will be a `patch` that's published to npm under the `latest` dist-tag. Any other branch will be a `prelease` that's published to npm under the `alpha` dist-tag.
-bump='patch'
-tag='latest'
-
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-default_branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
-
-if [ "$current_branch" != "$default_branch" ]; then
-  bump='prerelease'
-  tag='alpha'
-  npm --no-git-tag-version version $bump --preid=$tag
-else
-  npm version $bump
-fi
-
-# Push and publish!
-git push
-
-if [ "$tag" = 'alpha' ]; then
-  npm publish --tag $tag
-else
-  git push --tags
-  npm publish --tag $tag
-fi
+git push;
+git push --tags;
+npm publish;
