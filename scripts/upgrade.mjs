41534ef007f8da95742164f9942257e48dfbafa8
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-09 12:51:29 -0700

    Conditionally run npx npm-check-updates

--- scripts/upgrade.mjs
+++ scripts/upgrade.mjs
@@ -2,4 +2,12 @@
 
 import { $, argv } from 'zx';
 
+const { MODULE } = argv;
+
 await $`grabthar-validate-git`;
+
+if (!MODULE) {
+  $`npx npm-check-updates --registry='http://registry.npmjs.org' --dep=prod --upgrade`;
+} else {
+  $`npx npm-check-updates --registry='http://registry.npmjs.org' --dep=prod --upgrade --filter=${ MODULE }`;
+}
