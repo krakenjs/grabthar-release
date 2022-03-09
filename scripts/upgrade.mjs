010ae66ce24f85f9bb0813de7dad11ac0132f670
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-09 13:20:35 -0700

    Throw error if package-lock.json is missing

--- scripts/upgrade.mjs
+++ scripts/upgrade.mjs
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
 import { $, argv } from 'zx';
+import { cwd } from 'process';
+import { createRequire } from 'module';
 
 const { MODULE } = argv;
+const CWD = cwd();
+const moduleMetaUrl = import.meta.url;
+const require = createRequire(moduleMetaUrl);
+const fs = require('fs');
 
 await $`grabthar-validate-git`;
 
@@ -17,3 +23,9 @@ await $`rm -f ./package-lock.json;`;
 
 await $`npm install`;
 await $`npm test`;
+
+const PACKAGE_LOCK = `${ CWD }/package-lock.json`;
+
+if (!fs.existsSync(PACKAGE_LOCK)) {
+  throw new Error('Expected package-lock.json to be generated - are you using npm5+?');
+}
