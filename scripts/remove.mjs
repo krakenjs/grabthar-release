0670fffff3f832bc8d47956d68512b860eabeae8
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-09 15:13:22 -0700

    Validate and uninstall module

--- scripts/remove.mjs
+++ scripts/remove.mjs
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
 
-import { $ } from 'zx';
+import { $, argv } from 'zx';
+
+const { MODULE } = argv;
 
 await $`grabthar-validate-git`;
+
+if (!MODULE) {
+  throw new Error('Please provide a module name to remove');
+} else {
+  try {
+    await $`npm ls ${ MODULE }`
+  } catch (error) {
+    throw new Error(`${ MODULE } is not currently a dependency`);
+  }
+  await $`npm uninstall ${ MODULE }`;
+}
