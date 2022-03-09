a8e1571d7a4101ddb8036364b74a1d27356ef1c5
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-09 16:33:02 -0700

    Check for unstaged or uncommitted changes

--- publish.mjs
+++ publish.mjs
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+import { $ } from 'zx';
+
+try {
+    await $`git diff-files --quiet`;
+    await $`git diff-index --quiet --cached HEAD`;
+} catch (err) {
+    throw new Error(`Cannot continue with unstaged or uncommitted changes\nExit code: ${ err.exitCode }`);
+}
