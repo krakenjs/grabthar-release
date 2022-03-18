4f6c3a3763a1254b907e1d4bdbd1fdb19bf91b2a
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-18 12:06:43 -0700

    Throw error when publishing from a fork

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -8,12 +8,23 @@ import { $, question } from 'zx';
 
 const moduleMetaUrl = import.meta.url;
 const require = createRequire(moduleMetaUrl);
+
 let { NPM_TOKEN } = env;
+NPM_TOKEN = NPM_TOKEN || '';
+
 let BUMP = 'patch';
 let DIST_TAG = 'latest';
 let twoFactorCode;
 
-NPM_TOKEN = NPM_TOKEN || '';
+let { stdout: REMOTE_URL } = await $`git config --get remote.origin.url`;
+REMOTE_URL = REMOTE_URL.trim();
+
+const CWD = cwd();
+const { version: LOCAL_VERSION, repository: { url: REPO_URL } } = require(`${ CWD }/package.json`);
+
+if (REMOTE_URL !== REPO_URL) {
+    throw new Error('The remote url does not match the repo url. Publishing from a fork is not allowed.');
+}
 
 // This will determine the type of release based on the git branch. When the default branch is used, it will be a `patch` that's published to npm under the `latest` dist-tag. Any other branch will be a `prelease` that's published to npm under the `alpha` dist-tag.
 
@@ -44,10 +55,6 @@ if (NPM_TOKEN) {
 
 await $`git checkout package.json`;
 await $`git checkout package-lock.json || echo 'Package lock not found'`;
-
-const CWD = cwd();
-const { version: LOCAL_VERSION } = require(`${ CWD }/package.json`);
-
 await $`grabthar-verify-npm-publish --LOCAL_VERSION=${ LOCAL_VERSION } --DIST_TAG=${ DIST_TAG }`;
 
 // update non-prod dist tags whenever the latest dist tag changes
