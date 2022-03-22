2744dd36e46cfe17ff0d2536c732a3b933e929d0
Nate Bierdeman <nbierdeman@paypal.com>
2022-03-22 11:58:10 -0700

    `alpha` release optimizations (#27)
    * Skip version commit and tag, add preid for alpha releases
    
    * Push git tags for latest releases only

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -53,11 +53,17 @@ DEFAULT_BRANCH = DEFAULT_BRANCH.trim();
 if (CURRENT_BRANCH !== DEFAULT_BRANCH) {
     BUMP = 'prerelease';
     DIST_TAG = 'alpha';
+    await $`npm --no-git-tag-version version ${ BUMP } --preid=${ DIST_TAG }`;
+} else {
+    await $`npm version ${ BUMP }`;
 }
 
-await $`npm version ${ BUMP }`;
 await $`git push`;
-await $`git push --tags`;
+
+if (DIST_TAG === 'latest') {
+    await $`git push --tags`;
+}
+
 await $`grabthar-flatten`;
 
 if (NPM_TOKEN) {
